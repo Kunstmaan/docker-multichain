249d6b1f678108c5aa345b23cb9fc174c55c44bf
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-02-11 10:17:31 +0100

    Add the Multichain explorer

---
+++ explorer/runservices.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+sleep 30
+
+ip=`getent hosts masternode | awk -F' ' '{print $1}'`
+multichaind dockerchain@$ip:7447 -daemon
+
+sleep 30
+
+echo "rpcport=8000" >> /root/.multichain/dockerchain/multichain.conf
+cp /root/.multichain/dockerchain/multichain.conf /root/.multichain/multichain.conf
+
+python -m Abe.abe --config /root/explorer.conf --commit-bytes 100000 --no-serve
+python -m Abe.abe --config /root/explorer.conf &
+
+bash
--- node/connect-node.sh
+++ node/connect-node.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
 
+sleep 30
+
 ip=`getent hosts masternode | awk -F' ' '{print $1}'`
 multichaind dockerchain@$ip:7447 -printtoconsole
