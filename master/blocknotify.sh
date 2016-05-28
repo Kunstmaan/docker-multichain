136574ae790621673ffe2af183161af015c79bd9
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-05-28 13:25:28 +0200

    tune script

--- master/blocknotify.sh
+++ master/blocknotify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "{ 'block_hash': '$1' }" $APIURL
+echo '{ "block_hash": "'$1'" }'
+curl -sS -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "block_hash": "'$1'" }' $APIURL
